d6215fb6009b052546ff7f2989c5bbc958bf882e
Viktor Sárkzöi <s.wictor898@gmail.com>
2017-01-26 12:05:26 +0100

    Step002

---
+++ src/main/js/api/uriTemplateInterceptor.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Lenovo on 2017. 01. 26..
+ */
+define(function(require){
+    'use strict';
+    var interceptor = require('rest/interceptor');
+
+    return interceptor({
+        request: function(request){
+            if(request.path.indexOf('{')===-1){
+                return request;
+            } else{
+                request.path= request.path.split('{')[0];
+                return request;
+            }
+        }
+    });
+});
